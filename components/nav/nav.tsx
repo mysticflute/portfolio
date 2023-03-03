e981d2ada3097a5cec56fa925c4fe119c011e804
Nathan McWilliams <mysticflute@users.noreply.github.com>
2023-03-03 01:02:22 -0500

    fonts

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,45 +1,54 @@
-// import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 import styles from './nav.module.css';
 
 import logo from '../../public/images/logo.svg';
 
+const menu: { label: string; path: string }[] = [
+  { label: 'Home', path: '/' },
+  { label: 'About', path: '/about' },
+  { label: 'Portfolio', path: '/portfolio' },
+];
+
 export default function Nav() {
+  const { asPath: currentPage } = useRouter();
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.headerLeft}>
+      <div className={styles.content}>
+        <div className={styles.contentLeft}>
           <Link href="/" aria-label="home">
             <Image src={logo} alt="Site logo" className={styles.logo} />
           </Link>
         </div>
-        <div className={styles.headerMiddle}>
-          <nav role="navigation">
-            <ul role="list" className="header-nav-menu-list">
-              <li className="header-nav-list-item middle">
-                <a
-                  href="/home"
-                  aria-current="page"
-                  className="header-nav-link w-nav-link w--current"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="header-nav-list-item middle">
-                <a href="/about" className="header-nav-link w-nav-link">
-                  About
-                </a>
-              </li>
-              <li className="header-nav-list-item middle">
-                <a href="/portfolio" className="header-nav-link w-nav-link">
-                  Portfolio
-                </a>
-              </li>
+
+        <div className={styles.contentMiddle}>
+          <nav role="navigation" aria-label="Main navigtion">
+            <ul className={styles.list}>
+              {menu.map(item => {
+                const isCurrentPage = item.path === currentPage;
+                return (
+                  <li key={item.label}>
+                    <Link
+                      href={item.path}
+                      aria-current={isCurrentPage ? 'page' : undefined}
+                    >
+                      {item.label}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </nav>
         </div>
-        <div className={styles.headerRight}></div>
+
+        <div className={styles.contentRight}>
+          <Link href="/contact" className={styles.contact}>
+            <span className="lineRoundedIcons"></span>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import { Onest, LineRoundedIcons } from '@/lib/fonts';
+
 import 'normalize.css/normalize.css';
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-regular: ${Onest.style.fontFamily};
+          --font-icons-rounded: ${LineRoundedIcons.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
