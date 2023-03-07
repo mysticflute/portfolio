36202dec4be4834c8a4f215f370ca5e8ae80ab19
Nathan McWilliams <mysticflute@users.noreply.github.com>
2023-03-07 01:23:13 -0500

    navbar and overlay

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -3,91 +3,81 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { clsx } from 'clsx';
-
 import styles from './nav.module.css';
+import logoImage from '../../public/images/logo.svg';
 
-import logo from '../../public/images/logo.svg';
+type NavItems = {
+  label: string;
+  path: string;
+};
 
-const menu: { label: string; path: string }[] = [
+const mainNav: NavItems[] = [
   { label: 'Home', path: '/' },
   { label: 'About', path: '/about' },
   { label: 'Portfolio', path: '/portfolio' },
   { label: 'Contact', path: '/contact' },
 ];
 
+function createNav(items: NavItems[], currentItem: string) {
+  return (
+    <nav role="navigation" aria-label="Main navigation">
+      <ul className={styles.list}>
+        {items.map(item => (
+          <li key={item.label}>
+            <Link
+              href={item.path}
+              aria-current={item.path === currentItem ? 'page' : undefined}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
 export default function Nav() {
   const { asPath: currentPage } = useRouter();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
 
   return (
     <div className={styles.container}>
       <div className={styles.bar}>
         <div className={styles.barLeft}>
-          <Link href="/" aria-label="home" className={styles.logo}>
-            <Image src={logo} alt="Site logo" priority />
+          <Link href="/" aria-label="Home" className={styles.logo}>
+            <Image src={logoImage} alt="Site logo" priority />
           </Link>
         </div>
 
         <div className={styles.barMiddle}>
-          <nav role="navigation" aria-label="Main navigtion">
-            <ul className={styles.list}>
-              {menu.map(item => {
-                const isCurrentPage = item.path === currentPage;
-                return (
-                  <li key={item.label}>
-                    <Link
-                      href={item.path}
-                      aria-current={isCurrentPage ? 'page' : undefined}
-                    >
-                      {item.label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
+          {createNav(mainNav, currentPage)}
         </div>
 
         <div className={styles.barRight}>
-          <Link href="/contact" className={styles.contactButton}>
+          <Link href="/contact" className={styles.contact}>
             <span className="lineRoundedIcons"></span>
           </Link>
 
-          <div
-            role="button"
-            aria-label="menu"
+          <button
+            aria-label="Menu"
             aria-haspopup="menu"
             aria-controls="nav-overlay"
-            aria-expanded={isMenuOpen}
-            className={clsx(styles.hamburger, isMenuOpen && styles.open)}
-            onClick={() => setIsMenuOpen(isOpen => !isOpen)}
+            aria-expanded={isOverlayOpen}
+            className={clsx(styles.hamburger, isOverlayOpen && styles.open)}
+            onClick={() => setIsOverlayOpen(isOpen => !isOpen)}
           >
             <div className={styles.hamburgerTop}></div>
             <div className={styles.hamburgerBottom}></div>
-          </div>
+          </button>
         </div>
       </div>
 
-      <div className={clsx(styles.overlay, isMenuOpen && styles.open)}>
-        {true && (
-          <nav role="navigation" aria-label="Main navigtion">
-            <ul className={styles.overlayList}>
-              {menu.map(item => {
-                const isCurrentPage = item.path === currentPage;
-                return (
-                  <li key={item.label}>
-                    <Link
-                      href={item.path}
-                      aria-current={isCurrentPage ? 'page' : undefined}
-                    >
-                      {item.label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        )}
+      <div
+        id="nav-overlay"
+        className={clsx(styles.overlay, isOverlayOpen && styles.open)}
+      >
+        {createNav(mainNav, currentPage)}
       </div>
     </div>
   );
