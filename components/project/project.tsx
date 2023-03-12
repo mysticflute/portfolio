0d736bb5394528ab5fc4c2ad3ff00f8243ec717a
Nathan McWilliams <mysticflute@users.noreply.github.com>
2023-03-12 18:09:12 -0400

    cleanup soundcloud

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,31 +1,10 @@
+import { getUrlForTrack } from '@/lib/soundcloud';
 import styles from './project.module.css';
 
 import { type ProjectMetadata } from '@/lib/projects';
 
 // TODO: handle the image sizes correctly.
 /* eslint-disable @next/next/no-img-element */
-
-const baseSoundCloudURL =
-  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks';
-
-type SoundCloudOptions = {
-  /** Shows a black background when true. */
-  inverse?: boolean;
-  auto_play?: boolean;
-  show_user?: boolean;
-  color?: string;
-};
-
-// ('&inverse=false&auto_play=false&show_user=false');
-function getSoundCloudURL(id: number, options: SoundCloudOptions) {
-  let url = `${baseSoundCloudURL}/${id}&inverse=false&auto_play=false&show_user=false`;
-
-  // const formattedOptions = Object.keys(options).reduce(
-  //   (previous, key) => previous + `${key}=${options[key]}`
-  // );
-
-  return url;
-}
 
 type Props = {
   /**
@@ -60,7 +39,7 @@ export default function Project({ projectMetadata: data }: Props) {
                     width="100%"
                     height="20"
                     loading="lazy"
-                    src={getSoundCloudURL({ id, color: data.color })}
+                    src={getUrlForTrack(id, { color: data.color })}
                   ></iframe>
                 ))}
             </div>
