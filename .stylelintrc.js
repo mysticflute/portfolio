96c7cbb7b43ef037416e3695aa9e941fb7922bec
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-02-23 17:22:20 -0500

    Add Stylelint (#22)

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'alpha-value-notation': null,
+    'media-feature-range-notation': null,
+    'no-descending-specificity': null,
+    'selector-max-id': 0,
+    'selector-class-pattern': [
+      '^([a-z][a-zA-Z0-9]*)$',
+      {
+        message: selector =>
+          `Expected class selector "${selector}" to be camelCase`,
+      },
+    ],
+  },
+};
