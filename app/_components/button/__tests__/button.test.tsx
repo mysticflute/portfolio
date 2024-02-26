d74f29504922d455fab961789abe7e46601920b2
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-02-26 17:29:03 -0500

    button cmp test

---
+++ app/_components/button/__tests__/button.test.tsx
@@ -0,0 +1,70 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe, expect, it } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import Button from '../button';
+
+describe('button', () => {
+  it('renders a link', () => {
+    render(
+      <Button href="/foo" type="primary">
+        foo
+      </Button>,
+    );
+
+    expect(screen.getByRole('link')).toBeDefined();
+  });
+
+  it('renders the link url', () => {
+    render(
+      <Button href="/foo" type="primary">
+        foo
+      </Button>,
+    );
+
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/foo');
+  });
+
+  it('renders the link text', () => {
+    render(
+      <Button href="/foo" type="primary">
+        foo
+      </Button>,
+    );
+
+    expect(screen.getByRole('link')).toHaveTextContent('foo');
+  });
+
+  it('renders nested child content', () => {
+    render(
+      <Button href="/foo" type="primary">
+        <span className="icon"></span>
+        foo
+      </Button>,
+    );
+
+    expect(screen.getByRole('link')).toContainHTML('<span class="icon" />');
+  });
+
+  it('renders the link with type=primary', () => {
+    render(
+      <Button href="/foo" type="primary">
+        foo
+      </Button>,
+    );
+
+    expect(screen.getByRole('link')).toHaveClass('primary');
+  });
+
+  it('renders the link with type=secondary', () => {
+    render(
+      <Button href="/foo" type="secondary">
+        foo
+      </Button>,
+    );
+
+    expect(screen.getByRole('link')).toHaveClass('secondary');
+  });
+});
