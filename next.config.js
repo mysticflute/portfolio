1b60387140643f738b4065a95b85c7831c15558a
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-03-13 19:21:20 -0400

    cache files with version querystring

--- next.config.js
+++ next.config.js
@@ -6,10 +6,17 @@ const nextConfig = {
     return [
       {
         source: '/images/:path*(svg|jpg|png|webp)',
-        locale: false,
+        has: [
+          {
+            type: 'query',
+            key: 'v',
+            value: '([0-9]+)',
+          },
+        ],
         headers: [
           {
             key: 'Cache-Control',
+            // day * 86400 = seconds. e.g., 2592000 = 30 days
             value: 'public, max-age=2592000, immutable',
           },
         ],
