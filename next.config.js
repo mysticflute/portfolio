8eb595b749e2cacbbd020690e0f118876ec60a6c
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-03-14 13:05:15 -0400

    Add cache-control headers for images (#26)
    Add a cache-control header for images with a `v` querystring value,
    for example `/images/bg.png?v=1`. This doesn't apply to statically
    imported images used with `next/image`.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/images/:path*(svg|jpg|png|webp)',
+        has: [
+          {
+            type: 'query',
+            key: 'v',
+            value: '([0-9]+)',
+          },
+        ],
+        headers: [
+          {
+            key: 'Cache-Control',
+            // day * 86400 = seconds. e.g., 2592000 = 30 days
+            value: 'public, max-age=2592000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
