fe5cca04fcacb8102277501f0f66ff66054cd43d
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-03-18 16:39:38 -0400

    Optimize images (#28)
    - further optimizations for background and profile images.
    - `ColorSchemeImage` now uses `picture` and `source` elements.
    - add the `sharp` package for `next/image` optimization.
    - update dependency versions.

---
+++ app/_components/colorSchemeImageV1/__tests__/colorSchemeImageV1.test.tsx
@@ -0,0 +1,25 @@
+import { describe, expect, it } from '@jest/globals';
+import { render } from '@testing-library/react';
+import ColorSchemeImage from '../colorSchemeImageV1';
+
+const srcLight = {
+  src: '/srcLight.png',
+  height: 1,
+  width: 1,
+};
+
+const srcDark = {
+  src: '/srcDark.png',
+  height: 1,
+  width: 1,
+};
+
+describe('colorSchemeImage', () => {
+  it('matches the snapshot', () => {
+    const { container } = render(
+      <ColorSchemeImage srcLight={srcLight} srcDark={srcDark} alt="alt text" />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
