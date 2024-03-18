a82d1d9e93745de7a38e20b30636f64ba4c15ffa
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-03-18 16:39:38 -0400

    Optimize images (#28)
    - further optimizations for background and profile images.
    - `ColorSchemeImage` now uses `picture` and `source` elements.
    - add the `sharp` package for `next/image` optimization.
    - update dependency versions.

--- app/_components/colorSchemeImage/__tests__/colorSchemeImage.test.tsx
+++ app/_components/colorSchemeImage/__tests__/colorSchemeImage.test.tsx
@@ -1,23 +1,44 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it, beforeEach, jest } from '@jest/globals';
 import { render } from '@testing-library/react';
 import ColorSchemeImage from '../colorSchemeImage';
 
 const srcLight = {
   src: '/srcLight.png',
-  height: 1,
-  width: 1,
+  width: 200,
+  height: 100,
 };
 
 const srcDark = {
   src: '/srcDark.png',
-  height: 1,
-  width: 1,
+  width: 200,
+  height: 100,
 };
 
 describe('colorSchemeImage', () => {
-  it('matches the snapshot', () => {
+  beforeEach(() => {
+    // suppress React warning about fetchPriority prop until this is fixed:
+    // https://github.com/facebook/react/issues/27233
+    jest.spyOn(console, 'error').mockImplementation(jest.fn());
+  });
+
+  it('matches the snapshot with basic props', () => {
     const { container } = render(
-      <ColorSchemeImage srcLight={srcLight} srcDark={srcDark} alt="alt text" />,
+      <ColorSchemeImage srcDark={srcDark} srcLight={srcLight} alt="alt text" />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('matches the snapshot with additional props', () => {
+    const { container } = render(
+      <ColorSchemeImage
+        srcDark={srcDark}
+        srcLight={srcLight}
+        priority
+        quality={80}
+        alt="alt text"
+        sizes="(max-width: 500px) 100vw, 50vw"
+      />,
     );
 
     expect(container).toMatchSnapshot();
