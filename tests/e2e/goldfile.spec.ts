4bc8d18c4e9ea108d1453e3a3699ba3aa590201c
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-03-26 23:13:02 -0400

    remove goldfile test (really)

--- tests/e2e/goldfile.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('TMP test', () => {
-  test('matches screenshot', async ({ page }) => {
-    await page.goto('/');
-
-    await expect(page).toHaveScreenshot({ fullPage: true });
-  });
-});
