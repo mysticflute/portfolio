ce5052ff70eff31774bf6c8b21c132eecaa1f981
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-03-29 18:57:46 -0400

    tests

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import Layout from '@/components/layout/layout';
 import NotFound from '@/components/notFound/notFound';
+
+export const metadata: Metadata = {
+  title: 'Page Not Found',
+};
 
 export default function NotFoundPage() {
   return (
