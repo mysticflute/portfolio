543e3388e2fde19f0c087118ea515e40795f1c74
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-05-02 14:29:04 -0400

    e2e tests for audio

--- tests/e2e/audio.spec.ts
+++ tests/e2e/audio.spec.ts
@@ -17,4 +17,48 @@ test.describe('audio tracks', () => {
     await await track.getByRole('button', { name: 'Pause' }).click();
     await expect(audio).toHaveJSProperty('paused', true);
   });
+
+  test('only play one at a time', async ({ page }) => {
+    await page.goto('/');
+
+    const track1 = await page.getByTestId('audio-track').nth(0);
+    const track2 = await page.getByTestId('audio-track').nth(1);
+    const track3 = await page.getByTestId('audio-track').last();
+
+    const audio1 = await track1.locator('audio');
+    const audio2 = await track2.locator('audio');
+    const audio3 = await track3.locator('audio');
+
+    await track1.scrollIntoViewIfNeeded();
+    await track1.getByRole('button', { name: 'Play' }).click();
+    await expect(audio1).toHaveJSProperty('paused', false);
+
+    await track2.scrollIntoViewIfNeeded();
+    await track2.getByRole('button', { name: 'Play' }).click();
+    await expect(audio1).toHaveJSProperty('paused', true);
+    await expect(audio2).toHaveJSProperty('paused', false);
+
+    await track3.scrollIntoViewIfNeeded();
+    await track3.getByRole('button', { name: 'Play' }).click();
+    await expect(audio2).toHaveJSProperty('paused', true);
+    await expect(audio3).toHaveJSProperty('paused', false);
+  });
+
+  test('auto play the next track in the list', async ({ page }) => {
+    await page.goto('/');
+
+    const track1 = await page.getByTestId('audio-track').nth(0);
+    const track2 = await page.getByTestId('audio-track').nth(1);
+
+    const audio1 = await track1.locator('audio');
+    const audio2 = await track2.locator('audio');
+
+    await track1.scrollIntoViewIfNeeded();
+    await track1.getByRole('button', { name: 'Play' }).click();
+    await audio1.evaluate(
+      (el: HTMLAudioElement) => (el.currentTime = el.duration),
+    );
+
+    await expect(audio2).toHaveJSProperty('paused', false);
+  });
 });
