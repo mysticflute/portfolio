81d682f3f9faf896a6bf230749a599c124b5441c
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-05-06 18:00:48 -0400

    initial strip animation

--- app/_components/strip/strip.tsx
+++ app/_components/strip/strip.tsx
@@ -1,9 +1,46 @@
 import styles from './strip.module.css';
 
+const highlights = [
+  'Turning (Game - In Dev)',
+  'Recidivia (Game - In Dev)',
+  'Vulgaria (Game - In Dev)',
+  'D&D Commissions (Various)',
+  'Unity Asset Packs',
+  'Unreal Engine Asset Packs',
+];
+
 export default function Strip() {
   return (
-    <div className={styles.container}>
-      <div className={styles.strip}></div>
-    </div>
+    <section className={styles.container}>
+      <div className={styles.strip}>
+        {[...Array(4)].map((_, i) => (
+          <ul key={i} className={styles.stripContent} aria-hidden={i > 0}>
+            {highlights.map(highlight => (
+              <li
+                key={highlight}
+                className={`textHeadingXSmall ${styles.text}`}
+              >
+                {highlight}
+              </li>
+            ))}
+          </ul>
+        ))}
+
+        {/* <ul className={styles.stripContent} aria-hidden="true">
+          {highlights.map(highlight => (
+            <li key={highlight} className={`textHeadingXSmall ${styles.text}`}>
+              {highlight}
+            </li>
+          ))}
+        </ul> */}
+        {/* <ul className={styles.stripContent} aria-hidden="true">
+          {highlights.map(highlight => (
+            <li key={highlight} className={`textHeadingXSmall ${styles.text}`}>
+              {highlight}
+            </li>
+          ))}
+        </ul> */}
+      </div>
+    </section>
   );
 }
