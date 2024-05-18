590e68cfec2e23862bb2c386b5548375c5079b29
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-05-17 20:40:04 -0400

    e2e tests wip

--- tests/e2e/navigation.spec.ts
+++ tests/e2e/navigation.spec.ts
@@ -1,88 +1,95 @@
 import { test, expect, type Page } from '@playwright/test';
 
-// XXX: revisit some of these locators after improving accessibility of the
-// main nav bar. Probably will not have to use `first()` anymore, or as many
-// test ids.
 /** Common locators for the page. */
 const locators = {
-  /** The main navigation bar. */
-  navigationBar: {
+  /** The top navigation bar. */
+  mainNavigation: {
     /** The link containing the site logo image. */
-    siteLogo: (page: Page) => page.getByRole('banner').getByLabel('Home'),
+    siteLogo: (page: Page) =>
+      page
+        .getByRole('banner')
+        .getByRole('link', { name: 'Nathan David McWilliams' }),
 
     /** The "Home" text link. */
     homeLink: (page: Page) =>
-      page.getByTestId('nav-middle').getByRole('link', { name: 'Home' }),
+      page
+        .getByRole('banner') //
+        .getByRole('link', { name: 'Home' }),
 
     /** The "About" text link. */
     aboutLink: (page: Page) =>
-      page.getByTestId('nav-middle').getByRole('link', { name: 'About' }),
+      page
+        .getByRole('banner') //
+        .getByRole('link', { name: 'About' }),
 
     /** The "Portfolio" text link. */
     portfolioLink: (page: Page) =>
-      page.getByTestId('nav-middle').getByRole('link', { name: 'Portfolio' }),
+      page
+        .getByRole('banner') //
+        .getByRole('link', { name: 'Portfolio' }),
 
     /** The "Contact" text link. */
     contactLink: (page: Page) =>
-      page.getByTestId('nav-middle').getByRole('link', { name: 'Contact' }),
+      page
+        .getByRole('banner')
+        .getByRole('list')
+        .getByRole('link', { name: 'Contact' }),
 
     /** The link containing the contact icon. */
-    contactIcon: (page: Page) => page.getByRole('banner').getByLabel('Contact'),
-  },
+    contactIconLink: (page: Page) =>
+      page
+        .getByRole('banner') //
+        .getByRole('img', { name: 'Contact' }),
 
-  /** The main navigation overlay. */
-  navigationOverlay: {
     /** The button to open and close the nav overlay. */
-    button: (page: Page) =>
-      page.getByRole('banner').getByRole('button', { name: 'Menu' }),
-
-    /** The "Home" text link. */
-    homeLink: (page: Page) =>
-      page.getByTestId('nav-overlay').getByRole('link', { name: 'Home' }),
-
-    /** The "About" text link. */
-    aboutLink: (page: Page) =>
-      page.getByTestId('nav-overlay').getByRole('link', { name: 'About' }),
-
-    /** The "Portfolio" text link. */
-    portfolioLink: (page: Page) =>
-      page.getByTestId('nav-overlay').getByRole('link', { name: 'Portfolio' }),
-
-    /** The "Contact" text link. */
-    contactLink: (page: Page) =>
-      page.getByTestId('nav-overlay').getByRole('link', { name: 'Contact' }),
+    overlayButton: (page: Page) =>
+      page
+        .getByRole('banner')
+        .getByRole('button', { name: /(open|close) main menu/i }),
   },
 
   /** The site footer. */
   footer: {
     /** The link containing the site logo image. */
-    siteLogo: (page: Page) => page.getByTestId('footer-logo-link'),
+    siteLogo: (page: Page) =>
+      page
+        .getByRole('contentinfo')
+        .getByRole('link', { name: 'Nathan David McWilliams' }),
 
     /** The "Home" text link. */
-    homeLink: (page: Page) => page.getByRole('contentinfo').getByText('Home'),
+    homeLink: (page: Page) =>
+      page
+        .getByRole('contentinfo') //
+        .getByRole('link', { name: 'Home' }),
 
     /** The "About" text link. */
     aboutLink: (page: Page) =>
-      page.getByRole('contentinfo').getByRole('link', { name: 'About' }),
+      page
+        .getByRole('contentinfo') //
+        .getByRole('link', { name: 'About' }),
 
     /** The "Portfolio" text link. */
     portfolioLink: (page: Page) =>
-      page.getByRole('contentinfo').getByRole('link', { name: 'Portfolio' }),
+      page
+        .getByRole('contentinfo') //
+        .getByRole('link', { name: 'Portfolio' }),
 
     /** The "Contact" text link. */
     contactLink: (page: Page) =>
-      page.getByRole('contentinfo').getByRole('link', { name: 'Cosntact' }),
+      page
+        .getByRole('contentinfo') //
+        .getByRole('link', { name: 'Contact' }),
   },
 };
 
 test.describe('main navigation bar', { tag: '@desktop-only' }, () => {
   test('navigates across the website', async ({ page }) => {
-    const siteLogo = locators.navigationBar.siteLogo(page);
-    const homeLink = locators.navigationBar.homeLink(page);
-    const aboutLink = locators.navigationBar.aboutLink(page);
-    const portfolioLink = locators.navigationBar.portfolioLink(page);
-    const contactLink = locators.navigationBar.contactLink(page);
-    const contactIconButton = locators.navigationBar.contactIcon(page);
+    const siteLogo = locators.mainNavigation.siteLogo(page);
+    const homeLink = locators.mainNavigation.homeLink(page);
+    const aboutLink = locators.mainNavigation.aboutLink(page);
+    const portfolioLink = locators.mainNavigation.portfolioLink(page);
+    const contactLink = locators.mainNavigation.contactLink(page);
+    const contactIconButton = locators.mainNavigation.contactIconLink(page);
 
     await page.goto('/');
 
@@ -126,22 +133,20 @@ test.describe('main navigation bar', { tag: '@desktop-only' }, () => {
 
 test.describe('main navigation overlay', () => {
   test('navigates across the website', async ({ page, isMobile }) => {
-    const overlayButton = locators.navigationOverlay.button(page);
-    const homeLink = locators.navigationOverlay.homeLink(page);
-    const aboutLink = locators.navigationOverlay.aboutLink(page);
-    const portfolioLink = locators.navigationOverlay.portfolioLink(page);
-    const contactLink = locators.navigationOverlay.contactLink(page);
+    const overlayButton = locators.mainNavigation.overlayButton(page);
+    const homeLink = locators.mainNavigation.homeLink(page);
+    const aboutLink = locators.mainNavigation.aboutLink(page);
+    const portfolioLink = locators.mainNavigation.portfolioLink(page);
+    const contactLink = locators.mainNavigation.contactLink(page);
 
     if (!isMobile) {
       page.setViewportSize({ width: 390, height: 844 });
     }
 
     await page.goto('/');
-
-    await expect(homeLink).toHaveAttribute('aria-current', 'page');
+    await overlayButton.click();
 
     // click the About link
-    await overlayButton.click();
     await aboutLink.click();
 
     await expect(page).toHaveURL(/.*#about/);
