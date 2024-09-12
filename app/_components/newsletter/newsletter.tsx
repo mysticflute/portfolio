715325ab460544258db96c081605ebc341d9bb9f
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-09-12 15:43:12 -0400

    WIP

--- app/_components/newsletter/newsletter.tsx
+++ app/_components/newsletter/newsletter.tsx
@@ -1,8 +1,13 @@
-import { useId } from 'react';
+'use client';
+
+import { useId, useState, FormEvent } from 'react';
 import Image from 'next/image';
 import Box from '@/components/box/box';
+import Icon from '@/components/icon/icon';
 import subscribeImage from '@/public/images/illustrations/subscribe-large.svg';
 import styles from './newsletter.module.css';
+
+const endpoint = 'https://app.convertkit.com/forms/';
 
 const formId = process.env.NEXT_PUBLIC_NEWSLETTER_FORM_ID;
 if (!formId) {
@@ -13,6 +18,36 @@ if (!formId) {
 
 export default function Newsletter() {
   const id = useId();
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setIsSubmitting(true);
+
+    const formData = new FormData(event.currentTarget);
+
+    try {
+      const response = await fetch(`${endpoint}/${formId}/subscriptions`, {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Unable to submit newsletter registration: ${response.status} `,
+        );
+      }
+
+      setIsSuccess(true);
+    } catch (error) {
+      console.error(error);
+      setIsError(true);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
 
   return (
     <div className={styles.background}>
@@ -23,31 +58,47 @@ export default function Newsletter() {
 
         <Box type="flat" className={styles.box}>
           <span className="textHeadingXSmall">Subscribe to my newsletter</span>
+
           <div className={styles.form}>
-            <form
-              method="post"
-              action={`https://app.convertkit.com/forms/${formId}/subscriptions`}
-            >
-              <div className={styles.formElement}>
-                <label htmlFor={`${id}-email`} className="assistiveText">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  name="email_address"
-                  id={`${id}-email`}
-                  placeholder="Enter your email address"
-                  required
-                  maxLength={256}
-                  className={styles.input}
-                />
-                <input
-                  type="submit"
-                  value="Subscribe"
-                  className={styles.submit}
-                />
+            {!isSuccess && !isError && (
+              <form onSubmit={handleSubmit} aria-label="Newsletter signup">
+                <div className={styles.formElement}>
+                  <label htmlFor={`${id}-email`} className="assistiveText">
+                    Your email address
+                  </label>
+
+                  <input
+                    type="email"
+                    name="email_address"
+                    id={`${id}-email`}
+                    placeholder="Enter your email address"
+                    required
+                    maxLength={256}
+                    className={styles.input}
+                  />
+
+                  <button disabled={isSubmitting} className={styles.submit}>
+                    {isSubmitting ? 'Please wait...' : 'Submit'}
+                  </button>
+                </div>
+              </form>
+            )}
+
+            {isSuccess && (
+              <div className={styles.success}>
+                <Icon name="success" decorative className={styles.icon} />{' '}
+                Thanks for joining! Check your email to confirm.
               </div>
-            </form>
+            )}
+
+            <div role="alert" className={styles.error}>
+              {isError && (
+                <>
+                  <Icon name="error" decorative className={styles.icon} />{' '}
+                  Something went wrong! Please try again later.
+                </>
+              )}
+            </div>
           </div>
         </Box>
       </div>
