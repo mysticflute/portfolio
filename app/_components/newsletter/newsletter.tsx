7688d44926a5e6b6dccd80ce3daf3ab458e6575c
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-09-11 23:36:47 -0400

    use env varable

--- app/_components/newsletter/newsletter.tsx
+++ app/_components/newsletter/newsletter.tsx
@@ -4,6 +4,13 @@ import Box from '@/components/box/box';
 import subscribeImage from '@/public/images/illustrations/subscribe-large.svg';
 import styles from './newsletter.module.css';
 
+const formId = process.env.NEXT_PUBLIC_NEWSLETTER_FORM_ID;
+if (!formId) {
+  throw new Error(
+    'The environment variable NEXT_PUBLIC_NEWSLETTER_FORM_ID is not set.',
+  );
+}
+
 export default function Newsletter() {
   const id = useId();
 
@@ -19,7 +26,7 @@ export default function Newsletter() {
           <div className={styles.form}>
             <form
               method="post"
-              action="https://app.convertkit.com/forms/7081942/subscriptions"
+              action={`https://app.convertkit.com/forms/${formId}/subscriptions`}
             >
               <div className={styles.formElement}>
                 <label htmlFor={`${id}-email`} className="assistiveText">
