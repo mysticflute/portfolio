1ecba652e5d60d3b400fe3ec4c8e4a7158327ff1
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-09-11 23:13:17 -0400

    cleanup

--- app/_components/newsletter/newsletter.tsx
+++ app/_components/newsletter/newsletter.tsx
@@ -8,35 +8,36 @@ export default function Newsletter() {
   const id = useId();
 
   return (
-    <div className={styles.bg}>
+    <div className={styles.background}>
       <div className={styles.container}>
         <div className={styles.image}>
           <Image src={subscribeImage} alt="" />
         </div>
+
         <Box type="flat" className={styles.box}>
           <span className="textHeadingXSmall">Subscribe to my newsletter</span>
           <div className={styles.form}>
             <form
-              action="https://app.convertkit.com/forms/7081942/subscriptions"
               method="post"
+              action="https://app.convertkit.com/forms/7081942/subscriptions"
             >
-              <label htmlFor={`${id}-email`} className="assistiveText">
-                Email address
-              </label>
-              <div className={styles.inputWrapper}>
+              <div className={styles.formElement}>
+                <label htmlFor={`${id}-email`} className="assistiveText">
+                  Email address
+                </label>
                 <input
-                  id={`${id}-email`}
                   type="email"
                   name="email_address"
-                  maxLength={256}
-                  required
+                  id={`${id}-email`}
                   placeholder="Enter your email address"
+                  required
+                  maxLength={256}
                   className={styles.input}
                 />
                 <input
                   type="submit"
                   value="Subscribe"
-                  className={styles.submitButton}
+                  className={styles.submit}
                 />
               </div>
             </form>
